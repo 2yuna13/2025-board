0c76756e2c47356f505a33c96f517464841b811e
Yuna Lee <shha0213@gmail.com>
2025-03-02 14:51:18 +0900

    feat: 게시글 수 구현

--- service/article/src/main/java/board/article/controller/ArticleController.java
+++ service/article/src/main/java/board/article/controller/ArticleController.java
@@ -52,4 +52,10 @@ public class ArticleController {
     public void delete(@PathVariable Long articleId) {
         articleService.delete(articleId);
     }
+
+    //게시글 수 조회
+    @GetMapping("/v1/articles/boards/{boardId}/count")
+    public Long count(@PathVariable Long boardId) {
+        return articleService.count(boardId);
+    }
 }
--- service/article/src/main/java/board/article/service/ArticleService.java
+++ service/article/src/main/java/board/article/service/ArticleService.java
@@ -1,7 +1,9 @@
 package board.article.service;
 
 import board.article.entity.Article;
+import board.article.entity.BoardArticleCount;
 import board.article.repository.ArticleRepository;
+import board.article.repository.BoardArticleCountRepository;
 import board.article.service.request.ArticleCreateRequest;
 import board.article.service.request.ArticleUpdateRequest;
 import board.article.service.response.ArticlePageResponse;
@@ -18,12 +20,20 @@ import java.util.List;
 public class ArticleService {
     private final Snowflake snowflake = new Snowflake();
     private final ArticleRepository articleRepository;
+    private final BoardArticleCountRepository boardArticleCountRepository;
 
     @Transactional
     public ArticleResponse create(ArticleCreateRequest request) {
         Article article = articleRepository.save(
                 Article.create(snowflake.nextId(), request.getTitle(), request.getContent(), request.getBoardId(), request.getWriterId())
         );
+        //게시글 수 증가
+        int result = boardArticleCountRepository.increase(article.getBoardId());
+        if (result == 0) {
+            boardArticleCountRepository.save(
+                    BoardArticleCount.init(request.getBoardId(), 1L)
+            );
+        }
         return ArticleResponse.from(article);
     }
 
@@ -41,7 +51,10 @@ public class ArticleService {
 
     @Transactional
     public void delete(Long articleId) {
-        articleRepository.deleteById(articleId);
+        Article article = articleRepository.findById(articleId).orElseThrow();
+        articleRepository.delete(article);
+        // 게시글 수 감소
+        boardArticleCountRepository.decrease(article.getBoardId());
     }
 
     public ArticlePageResponse readAll(Long boardId, Long page, Long pageSize) {
@@ -60,5 +73,11 @@ public class ArticleService {
                 articleRepository.findAllInfiniteScroll(boardId, pageSize) :
                 articleRepository.findAllInfiniteScroll(boardId, pageSize, lastArticleId);
         return articles.stream().map(ArticleResponse::from).toList();
+    }
+
+    public Long count(Long boardId) {
+        return boardArticleCountRepository.findById(boardId)
+                .map(BoardArticleCount::getArticleCount)
+                .orElse(0L);
     }
 }
