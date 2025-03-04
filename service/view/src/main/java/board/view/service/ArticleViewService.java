22aefacfbf30b9badaa37e3aaf309ee57bb5b30d
Yuna Lee <shha0213@gmail.com>
2025-03-04 23:48:16 +0900

    feat: 조회수 어뷰징 방지 정책 구현

--- service/view/src/main/java/board/view/service/ArticleViewService.java
+++ service/view/src/main/java/board/view/service/ArticleViewService.java
@@ -1,18 +1,28 @@
 package board.view.service;
 
 import board.view.repository.ArticleViewCountRepository;
+import board.view.repository.ArticleViewDistributedLockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleViewService {
     private final ArticleViewCountRepository articleViewCountRepository;
     private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
+    private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
+
     private static final int BACK_UP_BACH_SIZE = 100;
+    private static final Duration TTL = Duration.ofMinutes(10);
 
     // 조회수 증가
     public Long increase(Long articleId, Long userId) {
+        if (!articleViewDistributedLockRepository.lock(articleId, userId, TTL)) {
+            return articleViewCountRepository.read(articleId);
+        }
+
         Long count = articleViewCountRepository.increase(articleId);
         if (count % BACK_UP_BACH_SIZE == 0) {
             articleViewCountBackUpProcessor.backup(articleId, count);
